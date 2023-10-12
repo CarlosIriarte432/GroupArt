5dc87d71ab4c989c3bebc744bfe4f5b499c49e58
Carlos Iriarte <carlosiriarte432@hotmail.com>
2023-10-12 17:52:19 -0300

    Se implementa Sidebar y CSS

--- Users/forms.py
+++ Users/forms.py
@@ -1,7 +1,19 @@
 # app_name/forms.py
 from django import forms
+from .models import User, UserType
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100, label="Ingrese su email")
     password = forms.CharField(widget=forms.PasswordInput, label="Ingrese su contraseña")
     remember_me = forms.BooleanField(required=False, widget=forms.CheckboxInput, label="Recuérdame")
+
+
+class UserRegistrationForm(forms.ModelForm):
+    user_type = forms.ModelChoiceField(queryset=UserType.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'full_name', 'rut', 'email', 'phone', 'address', 'privacy_settings']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
--- Users/views.py
+++ Users/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django import forms
 from django.contrib import messages
+from email import message
 
 # app_name/views.py
 from django.shortcuts import render, redirect
 from .forms import LoginForm
 from django.contrib.auth import authenticate, login
+from .models import User, UserType, PrivacySettings
+from .forms import UserRegistrationForm  
 
 def login_view(request):
     if request.method == 'POST':
@@ -37,9 +41,21 @@ def index(request):
     # Lógica de la vista
     return render(request, 'index.html')
 
-def register(request):
-    # Lógica de la vista
-    return render(request, 'index.html')
+def register_user(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            user_type_id = request.POST['user_type']  # Obtén el valor del tipo de usuario seleccionado en el formulario
+            user_type = get_object_or_404(UserType, pk=user_type_id)  # Obtiene la instancia de UserType
+            user = user_form.save(commit=False)
+            user.user_type = user_type  # Asigna la instancia de UserType
+            user.save()
+    else:
+        user_form = UserRegistrationForm()
+
+
+    return render(request, 'registration/register.html', {'user_form': user_form})
+
 
 def custom_logout(request):
     logout(request)
