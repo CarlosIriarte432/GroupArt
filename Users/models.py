d0fa782ffe8965e458ba4db821c236ea65f9dafa
Carlos Iriarte <carlosiriarte432@hotmail.com>
2023-10-14 20:28:43 -0300

    Login y Registro corregidos

--- Users/models.py
+++ Users/models.py
@@ -1,18 +1,34 @@
 
 # Create your models here.
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db import models
 # Modelo de Usuarios
-class User(models.Model):
-    username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=128)  # Almacenar la contraseña encriptada
+# class User(models.Model):
+#     username = models.CharField(max_length=50, unique=True)
+#     password = models.CharField(max_length=128)  # Almacenar la contraseña encriptada
+#     full_name = models.CharField(max_length=100)
+#     rut = models.CharField(max_length=20, unique=True)
+#     email = models.EmailField(unique=True)
+#     phone = models.CharField(max_length=20)
+#     address = models.TextField()
+#     privacy_settings = models.ForeignKey('PrivacySettings', on_delete=models.CASCADE)
+#     user_type = models.ForeignKey('UserType', on_delete=models.CASCADE)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=100)
     rut = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20)
-    address = models.TextField()
+    address = models.CharField(max_length=100)
     privacy_settings = models.ForeignKey('PrivacySettings', on_delete=models.CASCADE)
     user_type = models.ForeignKey('UserType', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
 
 # Modelo de Tipos de Usuario
 class UserType(models.Model):
